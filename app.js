673c014280d6a3873f9927f70f123350b30d8fc6
Programming-Knight <78259828+Programming-Knight@users.noreply.github.com>
2021-02-01 20:11:21 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-console.log('Hello-world');
+const form = document.getElementById('comments');
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    db.collection('comments').add({
+       name: form.name.value,
+       comment: form.comment.value
+    });
+    form.name.value = '';
+    form.comment.value = '';
+});
